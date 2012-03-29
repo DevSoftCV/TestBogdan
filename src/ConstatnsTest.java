5e5cfb551499e8c6bac0c802d754316d1c85e873
DevSoftCV <VolodimirM@gmail.com>
2012-03-29 12:47:36 +0300

    Update src/ConstatnsTest.java

--- src/ConstatnsTest.java
+++ src/ConstatnsTest.java
@@ -1,6 +1,6 @@
-
 public class ConstatnsTest {
 public static final int  VALUE1 = 1;
-public static final int  VALUE2 = 2;
+public static final int  VALUE2 = 2
+public static final int  VALUE3 = 3;
 public static final int  VALUE4 = 4;
 }
